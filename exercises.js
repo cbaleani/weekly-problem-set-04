41d7dd92b3999b54a323c48bcf1f40bf30e7ae24
Carla Baleani <carlabaleani27@gmail.com>
2020-10-24 15:32:21 -0300

    Exercise one: Variables and operators

---
+++ exercises.js
@@ -0,0 +1,20 @@
+/* 1. Variables y Operadores
+a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 
+   3er variable.*/
+var numOne = 10;
+var numTwo = 5;
+var numSum = numOne + numTwo;
+console.log(numSum);
+
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var hello = "Hello";
+var name = "John";
+var phrase = hello + " " + name;
+console.log(phrase);
+
+/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el 
+      resultado la suma una 3er variable (utilizar length).*/
+var drink = "tea";
+var food = "bread";
+var total = drink.length + food.length;
+console.log(total);
